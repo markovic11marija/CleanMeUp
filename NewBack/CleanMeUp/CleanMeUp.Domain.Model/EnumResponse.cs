a534bffe03054afcdfcc2dc7c27ecff47801522e
Marija Markovic <marija.markovic@nultien.rs>
2020-12-01 11:10:08 +0100

    adding enum response

---
+++ NewBack/CleanMeUp/CleanMeUp.Domain.Model/EnumResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanMeUp.Domain.Model
+{
+   public class EnumResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
