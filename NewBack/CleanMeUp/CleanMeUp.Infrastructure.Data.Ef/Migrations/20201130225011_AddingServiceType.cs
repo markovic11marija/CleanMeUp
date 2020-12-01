3dce2e028d2d06a42c77ed5ae1a51cf57ef0a011
Marija Markovic <marija.markovic@nultien.rs>
2020-12-01 13:12:57 +0100

    migration

---
+++ NewBack/CleanMeUp/CleanMeUp.Infrastructure.Data.Ef/Migrations/20201130225011_AddingServiceType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanMeUp.Infrastructure.Data.Ef.Migrations
+{
+    public partial class AddingServiceType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ServiceType",
+                table: "PriceList",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceType",
+                table: "Item",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceType",
+                table: "PriceList");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceType",
+                table: "Item");
+        }
+    }
+}
