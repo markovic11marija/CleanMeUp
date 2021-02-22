ea034771a80357dae20f7b1d11aceb26e35173fa
Marija Markovic <markovic11marija@gmail.com>
2021-02-22 23:25:49 +0100

    adding git ignore adding migrations

---
+++ CleanMeUp/CleanMeUp.Infrastructure.Data.Ef/Migrations/20210222222026_Email.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanMeUp.Infrastructure.Data.Ef.Migrations
+{
+    public partial class Email : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Order",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Order");
+        }
+    }
+}
